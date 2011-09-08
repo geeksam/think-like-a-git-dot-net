360aafe2945eacd7e9d89ab261184d8e3bf02fbd
Sam Livingston-Gray <geeksam@gmail.com>
2011-09-07 23:11:14 -0700

    Fix borrowed tag helper

--- lib/tag_helper.rb
+++ lib/tag_helper.rb
@@ -2,7 +2,7 @@ module TagHelper
   def tag(tag_name, tag_body, options = {})
     tag_name = tag_name.to_s
     tag_attrs = options.map { |k, v| %Q(#{k}="#{v}") }.join(' ')
-    start_tag = [tag_name, tag_attrs].reject(&:blank?).join(' ')
+    start_tag = [tag_name, tag_attrs].reject { |e| e =~ /^\s*$/ }.join(' ')
     end_tag = tag_name
     tag_body = (options[:format_string] % tag_body) if options[:format_string]
     %Q[<#{start_tag}>#{tag_body}</#{end_tag}>]
