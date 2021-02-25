aba731f01f3440f8d0f31924427b63e33f16e697
Zigmars Rupenheits <zigmarrs@gmail.com>
2021-02-25 15:04:50 +0200

    feat: vim nav with j/k within page
    Changed the behaviour of j/k keys to function as up down keys until the
    end/start of the page is reached, in which case the next/prev page is
    loaded as in original version.

--- source/assets/js/section.js
+++ source/assets/js/section.js
@@ -2,16 +2,34 @@ function nav_hotkey(e) {
   var action_for_key = function(e) {
     switch (e.keyCode) {
       case 74:  // 'j'
+        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+          return 'next';
+        } else {
+          return 'down';
+        }
       case 78:  // 'n'
         return 'next';
       case 75:  // 'k'
+        if (window.scrollY <= document.body.scrollTop) {
+          return 'prev'
+        } else {
+          return 'up';
+        }
       case 80:  // 'p'
         return 'prev';
     }
   }
+
+  const scrollStep = 50;
   var action = action_for_key(e);
   if (action == 'next' || action == 'prev') {
     var link = $('link[rel="'+action+'"]');
     window.location.href = link[0].href;
+  } else if (action == 'up'){
+    window.scrollTo(document.body.scrollLeft,
+      window.scrollY - scrollStep);
+  } else if (action == 'down'){
+    window.scrollTo(document.body.scrollLeft,
+      window.scrollY + scrollStep);
   }
 }
