ee606c071329d72402bacef49070bccdcf2a7176
Zigmars Rupenheits <zigmarrs@gmail.com>
2021-03-07 21:08:51 +0200

    feat: improve vim-style navigation key bindings
    * use j/k keys to navigate within page,
    * use h/l keys to go to previous/next page.

--- source/assets/js/section.js
+++ source/assets/js/section.js
@@ -2,19 +2,19 @@ function nav_hotkey(e) {
   var action_for_key = function(e) {
     switch (e.keyCode) {
       case 74:  // 'j'
-        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-          return 'next';
-        } else {
+        if ((window.innerHeight + window.scrollY) < document.body.scrollHeight) {
           return 'down';
         }
-      case 78:  // 'n'
-        return 'next';
+        return '';
       case 75:  // 'k'
-        if (window.scrollY <= document.body.scrollTop) {
-          return 'prev'
-        } else {
+        if (window.scrollY > document.body.scrollTop) {
           return 'up';
         }
+        return '';
+      case 76:  // 'l'
+      case 78:  // 'n'
+        return 'next';
+      case 72:  // 'h'
       case 80:  // 'p'
         return 'prev';
     }
